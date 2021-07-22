6736d11243dd1d87c26f55197844431d8d58f0d6
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2021-07-22 16:49:31 +0900

    flexdash: Initial check-in (#3)

---
+++ cmd/flexdash/routes.go
@@ -0,0 +1,173 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"embed"
+	"fmt"
+	"html/template"
+	"io"
+	"math"
+	"net/http"
+	"strconv"
+
+	"github.com/alessio/shellescape"
+	"github.com/julienschmidt/httprouter"
+	"github.com/nya3jp/flex"
+	"golang.org/x/sync/errgroup"
+)
+
+//go:embed templates
+var templatesFS embed.FS
+
+var funcs = template.FuncMap{
+	"shellquote": shellescape.QuoteCommand,
+}
+
+var templateIndex = template.Must(template.New("index.html").Funcs(funcs).ParseFS(templatesFS, "templates/index.html", "templates/base.html"))
+var templateJobs = template.Must(template.New("jobs.html").Funcs(funcs).ParseFS(templatesFS, "templates/jobs.html", "templates/base.html"))
+var templateJob = template.Must(template.New("job.html").Funcs(funcs).ParseFS(templatesFS, "templates/job.html", "templates/base.html"))
+
+type jobsValues struct {
+	Jobs            []*flex.JobStatus
+	NextBeforeJobID int64
+}
+
+type jobValues struct {
+	Job            *flex.JobStatus
+	Stdout, Stderr string
+}
+
+func respond(w http.ResponseWriter, r *http.Request, f func(ctx context.Context) error) {
+	if err := f(r.Context()); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func renderHTML(w http.ResponseWriter, tmpl *template.Template, values interface{}) error {
+	w.Header().Set("Content-Type", "text/html")
+	return tmpl.Execute(w, values)
+}
+
+func readJobOutput(ctx context.Context, cl flex.FlexServiceClient, id *flex.JobId, t flex.GetJobOutputRequest_JobOutputType) (string, error) {
+	rres, err := cl.GetJobOutput(ctx, &flex.GetJobOutputRequest{Id: id, Type: t})
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, rres.GetLocation().GetPresignedUrl(), nil)
+	if err != nil {
+		return "", err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+type server struct {
+	cl flex.FlexServiceClient
+}
+
+func (s *server) handleIndex(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	respond(w, r, func(ctx context.Context) error {
+		return renderHTML(w, templateIndex, nil)
+	})
+}
+
+func (s *server) handleJobs(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	respond(w, r, func(ctx context.Context) error {
+		query := r.URL.Query()
+		beforeJobID := int64(math.MaxInt64)
+		if i, err := strconv.ParseInt(query.Get("before"), 10, 64); err == nil {
+			beforeJobID = i
+		}
+
+		res, err := s.cl.ListJobs(ctx, &flex.ListJobsRequest{Limit: 100, BeforeId: &flex.JobId{IntId: beforeJobID}})
+		if err != nil {
+			return err
+		}
+
+		jobs := res.GetJobs()
+		var nextBeforeJobID int64
+		if len(jobs) > 0 {
+			nextBeforeJobID = jobs[len(jobs)-1].GetJob().GetId().GetIntId()
+		}
+		values := &jobsValues{
+			Jobs:            jobs,
+			NextBeforeJobID: nextBeforeJobID,
+		}
+		return renderHTML(w, templateJobs, values)
+	})
+}
+
+func (s *server) handleJob(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	respond(w, r, func(ctx context.Context) error {
+		jobIntID, err := strconv.ParseInt(p.ByName("jobID"), 10, 64)
+		if err != nil {
+			return fmt.Errorf("invalid job ID: %w", err)
+		}
+		jobID := &flex.JobId{IntId: jobIntID}
+
+		res, err := s.cl.GetJob(ctx, &flex.GetJobRequest{Id: jobID})
+		if err != nil {
+			return err
+		}
+		job := res.GetJob()
+
+		var stdout, stderr string
+		if job.GetState() == flex.JobState_FINISHED {
+			g, ctx := errgroup.WithContext(ctx)
+			g.Go(func() error {
+				var err error
+				stdout, err = readJobOutput(ctx, s.cl, jobID, flex.GetJobOutputRequest_STDOUT)
+				return err
+			})
+			g.Go(func() error {
+				var err error
+				stderr, err = readJobOutput(ctx, s.cl, jobID, flex.GetJobOutputRequest_STDERR)
+				return err
+			})
+			if err := g.Wait(); err != nil {
+				return err
+			}
+		}
+
+		values := &jobValues{
+			Job:    job,
+			Stdout: stdout,
+			Stderr: stderr,
+		}
+		return renderHTML(w, templateJob, values)
+	})
+}
+
+func newRouter(cl flex.FlexServiceClient) *httprouter.Router {
+	srv := &server{cl: cl}
+	router := httprouter.New()
+	router.GET("/", srv.handleIndex)
+	router.GET("/jobs/", srv.handleJobs)
+	router.GET("/jobs/:jobID/", srv.handleJob)
+	return router
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	cloud.google.com/go v0.86.0
 	cloud.google.com/go/storage v1.16.0
+	github.com/alessio/shellescape v1.4.1
 	github.com/aws/aws-sdk-go-v2/config v1.5.0
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.11.1
 	github.com/aws/smithy-go v1.6.0
@@ -12,8 +13,10 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/go-cmp v0.5.6
 	github.com/google/uuid v1.1.2
+	github.com/julienschmidt/httprouter v1.3.0
 	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c
 	google.golang.org/api v0.50.0
 	google.golang.org/grpc v1.39.0
